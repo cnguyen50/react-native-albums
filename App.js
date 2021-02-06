bd8bad553cab55a7971a50cdc76de4dd262a5195
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-05 19:16:52 -0800

    imported Header component into app and haver it render

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Text } from 'react-native'
+import Header from './src/components/header'
 
 const App = () => {
   return (
-      <Text>Some text</Text>
+      <Header/>
   )
 }
 
