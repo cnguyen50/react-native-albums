74a7f2eb7d746c78ba38c951c6fc4e07233741c0
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-05 19:32:53 -0800

    imported View and wrapped text with background color

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 
 const Header = () => {
-  const { textStyle } = styles
+  const { textStyle, viewStyle } = styles
+  
   return (
-    <Text style={textStyle}>Albums</Text>
+    <View style={viewStyle}> 
+      <Text style={textStyle}>Albums</Text>
+    </View>
   )
 }
 
 const styles = {
   textStyle: {
     fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
   }
-
 }
 
 export default Header;
