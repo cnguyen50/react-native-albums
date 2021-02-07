5fb1e894937098c7024ab88a34e03b7464270bcc
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-06 21:34:25 -0800

    passed props obj to header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Text, View } from 'react-native'
 
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles
 
   return (
     <View style={viewStyle}> 
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   )
 }
@@ -24,7 +24,9 @@ const styles = {
     paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2},
-    shadowOpacity: 0.5
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative'
   }
 }
 
