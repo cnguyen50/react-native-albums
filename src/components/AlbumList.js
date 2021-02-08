891cbe8e42631b95045f6e44e906989e023083fc
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-08 15:03:17 -0800

    removed text

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import axios from 'axios'
 import AlbumDetail from './AlbumDetail'
 
@@ -14,8 +14,8 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(albums => 
-      <AlbumDetail key={album.title}>{album.title}</AlbumDetail>
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album}/>
     )
   }
 
