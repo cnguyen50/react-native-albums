f127d2d6e10750798875ca99db4110073aa51aa0
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-08 15:00:27 -0800

    boilerplate for album detail

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+const AlbumDetail = () => {
+  return (
+      <View>
+        <Text>Album Details</Text>
+      </View>
+  )
+}
+
+export default AlbumDetail;
