0ca1a834993eeaba5aafc4f43d53006e7c04286b
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-11 21:07:35 -0800

    styling for album image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,7 +5,13 @@ import CardSection from './CardSection'
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album
-  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle } = styles
+  const { 
+    thumbnailStyle, 
+    headerContentStyle, 
+    thumbnailContainerStyle, 
+    headerTextStyle, 
+    imageStyle 
+  } = styles
 
   return (
       <Card>
@@ -22,7 +28,10 @@ const AlbumDetail = ({ album }) => {
           </View>
         </CardSection>
         <CardSection>
-          <Image source={{ uri: image }}/>
+          <Image 
+            style={imageStyle} 
+            source={{ uri: image }}
+          />
         </CardSection>
       </Card>
       
@@ -46,6 +55,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
