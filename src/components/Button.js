91ef1b2e9d913a8c43c93a9e1bd756fdf46f0962
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-02-14 20:47:02 -0800

    deconstructed textstyles and passed into component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 
 const Button = () => {
-  const { buttonStyle } = styles
+  const { buttonStyle, textStyle } = styles
 
   return (
     <TouchableOpacity style={buttonStyle}>
-      <Text>Click me</Text>
+      <Text style={textStyle}>Click me</Text>
     </TouchableOpacity>
   )
 }
@@ -16,7 +16,9 @@ const styles = {
     alignSelf: 'center',
     color: '#007aff',
     fontSize: 16,
-    fontWeight:'600'
+    fontWeight:'600',
+    paddingTop: 10,
+    paddingBottom: 10
   },
   buttonStyle: {
     flex: 1,
